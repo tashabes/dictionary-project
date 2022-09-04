1d09c0d9ed5ed731f99bdd2dc2df1cdddf810470
Natasha Abel <natasha_abel@hotmail.co.uk>
2022-09-04 16:59:08 +0100

    Added in the Dictionary component with search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary App</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary App</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Natasha Abel</small>
+        </footer>
+      </div>
     </div>
   );
 }
