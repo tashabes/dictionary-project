2af298eb682c9f69f03010632f1dc4554e64a79f
Natasha Abel <natasha_abel@hotmail.co.uk>
2022-09-04 20:34:17 +0100

    Added an alt to images

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,12 @@ import "./phonetic.css";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+      <a
+        href={props.phonetic.audio}
+        target="_blank"
+        rel="noreferrer"
+        alt="Word image"
+      >
         Listen
       </a>
       <span className="text">{props.phonetic.text}</span>
