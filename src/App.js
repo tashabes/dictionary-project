600dbfecc4d4f87833c55a6f56365861aaf3199f
Natasha Abel <natasha_abel@hotmail.co.uk>
2022-09-04 16:28:40 +0100

    Title update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">
+        <h1>Dictionary App</h1>
+      </header>
     </div>
   );
 }
